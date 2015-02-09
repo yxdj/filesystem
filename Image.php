9fb4adad2ee7b7fe687bae68e42a1e70d5beb5d1
xuyuan <1184411413@qq.com>
2015-02-09 09:31:17 +0800

    add filesystem

---
+++ Image.php
@@ -0,0 +1,444 @@
+<?php
+namespace yxdj\filesystem;
+
+/*图像处理类
+$img=new self(source);//获得原图名柄（原图路径）
+$img->rgb(array(255,255,255));
+$img->size($dwidth=0,$dheight=0,$sizeType=self::CUT);//默认：原宽，原高，切割
+$img->save($destination);//包含保存格式
+$img->screen($type);//显示格式
+$img->delete();------------删除源图片
+$img->close();-------------关闭源图句柄
+实际类型和文件后缀是分开验证的，
+保存的时候路经是什么类型就实际是什么类型了
+
+处理过程：
+1.创建一个所要大小的画板
+2.在旧图中划出一个区域，在新图中也画出一个区域（不一定是画板大小）
+3.把旧图的区域，复制到新图中去。
+
+处理多次图片不方便，
+还有异常处理，参数传入，
+$img=new FlyImage();
+
+$img->set(PUBLIC_PATH.'23_1334916707.jpg');
+$img->save(200,200,[300,50,220,220],PUBLIC_PATH.'a1.png');
+
+
+$img->set(PUBLIC_PATH.'23_1416293682.jpg');
+$img->save(500,500,FlyImage::SET,PUBLIC_PATH.'b0.jpg');		
+$img->save(200,200,FlyImage::CUT,PUBLIC_PATH.'b1.jpg');
+$img->save(200,200,FlyImage::DEF,PUBLIC_PATH.'b2.jpg');
+$img->save(1000,200,FlyImage::MAX,PUBLIC_PATH.'b3.png');
+$img->save(200,200,[0,0,220,220],PUBLIC_PATH.'b4.png');
+$img->save(200,200,[200,30,220,220],PUBLIC_PATH.'b5.png');
+
+*/
+class Image {
+    const CUT=1;
+    const DEF=2;
+    const MAX=3;
+	const SET=4;
+	
+	
+	private $background=array(255,255,255);	//设置透明色，默认为黑色
+    public  $status;       //是否成功
+    public  $error;          //错误信息
+	
+	
+    private $width_pro;         //改变前后宽度的比例
+    private $height_pro;        //改变前后高度的比例
+    private $img;               //原图的资源句柄
+    private $new;               //新图的资源句柄
+	private $tt;          		//原图片类型（真实）	
+	private $st;          		//原图片类型（后缀）
+	private $dt;          		//新图片类型（后缀）
+
+	
+
+    private $source;            //原图片地址
+    private $destination;       //新图片地址
+    private $type;              //原图片类型（实际，数字）裁剪方式
+
+	
+	
+	
+	//原图
+    private $swidth;         //原图片宽度
+    private $sheight;        //原图片高度	
+    private $sx=0;       		//创建裁剪点X(默认为0不裁剪)     
+    private $sy = 0;   			//创建裁剪点Y(默认为0不裁剪) 
+    private $sw=200;     		//设定的裁剪W
+    private $sh=200;    		//设定的裁剪H	    
+	
+	
+	
+	//新图,只需要宽高就可，其它的已能计算得来，再给的话图片将达不成效果
+    private $dwidth;      		//容器宽度(输出，新图时的参数大小)
+    private $dheight;        	//容器高度(输出，新图时的参大小)
+    private $dx=0;       		//创建裁剪点X(默认为0不裁剪)     
+    private $dy=0;    			//创建裁剪点Y(默认为0不裁剪) 
+    private $dw;        	 	//新图片的宽度w(过渡，参数大小->扩大)
+    private $dh;        		//新图片的高度h(过渡，参数大小->扩大)    
+	
+    //构造方法，初始化
+    public function __construct($confs=array()){
+		$this->init();
+	}
+	
+	private function init(){
+		$this->status=false;
+		$this->error='init: cut image fail!';
+	}
+
+
+
+
+	//设定来源图片
+	public function set($source){
+		$this->init();
+		
+		//取得原图片地址->磁盘地址
+		if(!is_file($this->source=$source)){
+                $this->error='来源文件不存在';
+                return false;			
+		}
+
+		//取得图片的宽，高，类型
+		list($this->swidth, $this->sheight, $this->tt)=getimagesize($this->source);
+
+	
+		$type=$this->tt;
+        switch ($type) {
+            case 1 : $img = imagecreatefromgif($source);break;
+            case 2 : $img = imagecreatefromjpeg($source);break;
+            case 3 : $img = imagecreatefrompng($source);break;				
+            default:$this->error='图片处理类不能获取此图片: '.$source;return false;
+        }
+		$this->img=$img;
+        return true;
+	}
+	
+  
+    
+      
+    //向显示器输出图片
+    public function screen($dwidth = 0,$dheight = 0,$sizeType=self::CUT,$dt=null){
+        $this->init();
+		$this->size($dwidth,$dheight,$sizeType);
+		
+		
+		$info=pathinfo($this->source);//取得原后缀名
+        $this->st=$info['extension'];    
+        $dt==null?$this->dt=$this->st:$this->dt=$dt;
+        switch ($this->dt) {
+            case 'gif' :
+                $img = imagegif($this->new);
+                break;
+            case 'jpg' :
+                $img = imagejpeg($this->new);
+                break;
+            case 'jpeg' :
+                $img = imagejpeg($this->new);
+                break;				
+            case 'png' :
+                $img = imagepng($this->new);
+                break;
+            default:
+                $this->error='图片处理类不能生成此格式图片: '.$this->dt;
+                return false;
+        }
+        imagedestroy($this->new);//消毁新图资源句柄
+		$this->status=true;
+		return true;
+    }
+      
+    //保存图片
+    public function save($dwidth = 0,$dheight = 0,$sizeType=self::CUT,$destination=null){
+        $this->init();
+		$this->size($dwidth,$dheight,$sizeType);
+		
+		//不指定目标路径将覆盖原文件
+		$destination==null?$this->destination=$this->source:$this->destination=$destination;
+		$info=pathinfo($this->destination);
+        $this->dt=$info['extension'];
+		
+		if(!is_dir($info['dirname'])){
+			$this->status=false;
+			$this->error='图片所移至的目标路径不存在！';
+			return false;
+		}
+        switch ($this->dt) {
+            case 'gif' :
+                $img = imagegif($this->new,$this->destination);
+                break;
+            case 'jpg' :
+                $img = imagejpeg($this->new,$this->destination);
+                break;
+            case 'jpeg' :
+                $img = imagejpeg($this->new,$this->destination);
+                break;				
+            case 'png' :
+                $img = imagepng($this->new,$this->destination);
+                break;				
+            default:
+                $this->status=false;
+                $this->error='图片处理类不能生成此格式图片: '.$this->dt;
+                return false;
+        } 
+          
+        imagedestroy($this->new);//消毁新图资源句柄，只用一次，每一次都不一样
+		$this->status=true;
+		return true;
+    } 
+
+
+
+
+
+
+
+
+
+
+    //改变图片尺寸
+    private function size($dwidth = 0,$dheight = 0,$sizeType=self::CUT){
+        //分析传递的参数，宽和高
+        //宽高不是数字，宽高没设定，设定为0，---->原大小输出
+        if (!is_numeric($dwidth) || !is_numeric($dheight)||$dwidth==0||$dheight==0) {
+            $this->dwidth = $this->swidth;//新宽==原宽
+            $this->dheight = $this->sheight;//新高==原高
+        }else{  //其它情况则按传递的参数大小输出图片
+            $this->dwidth = $dwidth;//新宽==传宽
+            $this->dheight = $dheight;//新高==传高   
+        }
+		
+		$this->dw=$this->dwidth;
+		$this->dh=$this->dheight;
+		
+		if(is_array($sizeType)){
+			$this->sx=$sizeType[0];
+			$this->sy=$sizeType[1];
+			$this->sw=$sizeType[2];
+			$this->sh=$sizeType[3];
+			$this->dx=0;
+			$this->dy=0;
+			$sizeType=self::SET;
+		}else{
+			$this->sx=0;
+			$this->sy=0;
+			$this->sw=$this->swidth;
+			$this->sh=$this->sheight;			
+		}
+		
+        switch($sizeType){
+            case self::CUT: $this->cutSize();break;
+            case self::DEF: $this->defSize();break;
+            case self::MAX: $this->maxSize();break;
+			case self::SET: $this->setSize();break;
+			
+            default:
+                $this->error='图片处理类不能以此种方式处理图片: '.$sizeType;
+                return false;
+        }
+		return true;
+    } 
+
+
+
+	
+	
+	//设置背景色
+	public function background($rgb){
+		$this->background=$rgb;
+	}
+	
+	public function __set($name,$value){
+		$this->$name=$value;
+	}
+	
+	public function __get($name){
+		return $this->$name;
+	}
+
+
+
+	
+	
+    //不压缩变形，达到给定大小，会裁剪
+	/*
+	这里采用的是放大的方式，
+	还有一种缩小的方式，去原图中找点位
+	*/
+    public function cutSize2() {       
+        //得到参数，即以后要生成的图片的内容的大小
+        $this->dw = $this->dwidth;
+        $this->dh = $this->dheight;
+        $this->sw = $this->swidth;
+        $this->sh = $this->sheight;   
+  
+        //求比例，重设新图宽高，设裁剪点，此新图宽高是指从旧图拿出来的大小，当然是不能比旧图小  
+        $this->width_pro=$this->dwidth/$this->swidth;
+        $this->height_pro=$this->dheight/$this->sheight;
+        if($this->width_pro < $this->height_pro){
+            $this->dwidth =$this->swidth*$this->height_pro;//高的比例大，就让宽*大比例
+            $this->sx =-($this->dwidth - $this->dw)/2; //求出裁剪点的宽度  
+            $this->sy=0;
+        }else{
+			$this->dheight =$this->sheight*$this->width_pro;//宽的比例大，就让高*大比例    
+            $this->sy = -($this->dheight - $this->dh)/2; //求出裁剪点的高度  
+            $this->sx=0;
+        }
+        //创建新图
+        $this->new = imagecreatetruecolor($this->dw,$this->dh);
+		
+        //设置纯黑色为透明
+        $transparent= imagecolorallocate($this->new,$this->background[0],$this->background[1],$this->background[2]);
+        imagecolortransparent($this->new,$transparent);
+        imagefilledrectangle($this->new,0,0,$this->dw,$this->dh,$transparent);
+          
+          
+        //从旧图生成新图
+        //参数：新句柄，旧句柄，新图裁剪点XY,旧图裁剪点XY,新图（拿出来的图）大小，旧图大小
+        imagecopyresampled($this->new,$this->img,$this->sx,$this->sy,0,0,
+                           $this->dwidth,$this->dheight,$this->sw,$this->sh);
+    }
+      
+  
+ 
+	//压缩处理，和其它其种的裁剪方式就要统一点了
+    public function cutSize() {       
+        //得到参数，即以后要生成的图片的内容的大小
+        $this->dw = $this->dwidth;
+        $this->dh = $this->dheight;
+        $this->sw = $this->swidth;
+        $this->sh = $this->sheight; 
+
+  
+        //求比例，缩小
+        $this->width_pro=$this->swidth/$this->dwidth;
+        $this->height_pro=$this->sheight/$this->dheight;
+        if($this->width_pro > $this->height_pro){
+            $this->sw =$this->dwidth*$this->height_pro;//宽的比例大，就让宽*小比例
+            $this->sx =($this->swidth - $this->sw)/2; //求出裁剪点的宽度  
+            $this->sy=0;
+        }else{
+			$this->sh =$this->dheight*$this->width_pro;//高的比例大，就让高*小比例    
+            $this->sy =($this->sheight - $this->sh)/2; //求出裁剪点的高度  
+            $this->sx=0;
+        }
+        //创建新图
+        $this->new = imagecreatetruecolor($this->dwidth,$this->dheight);
+		
+        //设置纯黑色为透明
+        $transparent= imagecolorallocate($this->new,$this->background[0],$this->background[1],$this->background[2]);
+        imagecolortransparent($this->new,$transparent);
+        imagefilledrectangle($this->new,0,0,$this->dwidth,$this->dheight,$transparent);
+          
+          
+        //从旧图生成新图
+        //参数：新句柄，旧句柄，新图裁剪点XY,旧图裁剪点XY,新图（拿出来的图）大小，旧图大小
+        imagecopyresampled($this->new,$this->img,0,0,$this->sx,$this->sy,
+                           $this->dw,$this->dh,$this->sw,$this->sh);
+    }
+
+
+
+
+
+
+
+
+ 
+  
+    //压缩变形，达到给定大小，不裁剪
+    public function defSize() {       
+        //得到参数，即以后要生成的图片的内容的大小
+        $this->dw = $this->dwidth;
+        $this->dh = $this->dheight;
+        $this->sw = $this->swidth;
+        $this->sh = $this->sheight;
+		
+        //创建新图
+        $this->new = imagecreatetruecolor($this->dwidth,$this->dheight);
+          
+        //设置纯黑色为透明
+		$transparent= imagecolorallocate($this->new,$this->background[0],$this->background[1],$this->background[2]);
+        imagecolortransparent($this->new,$transparent);
+        imagefilledrectangle($this->new,0,0,$this->dwidth,$this->dheight,$transparent);
+          
+          
+        //从旧图生成新图
+        //参数：新句柄，旧句柄，新图裁剪点XY,旧图裁剪点XY,新图（拿出来的图）大小，旧图大小
+        imagecopyresampled($this->new,$this->img,0,0,0,0,
+                            $this->dw,$this->dh,$this->sw,$this->sh);
+    }
+      
+  
+  
+    //不压缩变形，不裁剪，在给定大小内最大化,以全景呈现
+    public function maxSize() {       
+        //把传递的参数赋给容器，即以后要生成的图片大小
+  
+        //求比例，重设新图宽高，设裁剪点，此新图宽高是指从旧图拿出来的大小，当然是不能比旧图大  
+        $this->width_pro=$this->dwidth/$this->swidth;
+        $this->height_pro=$this->dheight/$this->sheight;
+        if ($this->width_pro < $this->height_pro) {
+            $this->dh =$this->sheight*$this->width_pro;//宽的比例小，就让高*小比例    
+            //$this->sy = -($this->dheight - $this->dh)/2; //求出裁剪点的高度
+        } else {
+            $this->dw =$this->swidth*$this->height_pro;//高的比例小，就让宽*小比例
+            //$this->sx =-($this->dwidth - $this->dw)/2; //求出裁剪点的宽度        
+        }
+          
+        //创建新图,
+        $this->new = imagecreatetruecolor($this->dwidth,$this->dheight);
+          
+        //设置纯黑色为透明
+		$transparent= imagecolorallocate($this->new,$this->background[0],$this->background[1],$this->background[2]);
+        imagecolortransparent($this->new,$transparent);
+        imagefilledrectangle($this->new,0,0,$this->dwidth,$this->dheight,$transparent);
+  
+        //从旧图生成新图
+        //参数：新句柄，旧句柄，新图裁剪点XY,旧图裁剪点XY,新图（拿出来的图,等会再放到wrap里）大小，旧图大小
+        imagecopyresampled($this->new,$this->img,0,0,0,0,
+                            $this->dw,$this->dh,$this->sw,$this->sh);
+    }
+ 
+ 
+ 
+ 
+ 
+//--------------------------------------------------
+    //根据指定裁剪点，达到给定大小，裁剪，更贴合用户
+    public function setSize() {
+        //把传递的参数赋给容器，即以后要生成的图片大小	
+		
+        //创建新图
+        $this->new = imagecreatetruecolor($this->dwidth,$this->dheight);
+          
+        //设置纯黑色为透明
+        $transparent= imagecolorallocate($this->new,$this->background[0],$this->background[1],$this->background[2]);
+        imagecolortransparent($this->new,$transparent);
+        imagefilledrectangle($this->new,0,0,$this->dwidth,$this->dheight,$transparent);
+          
+        //从旧图生成新图
+        //参数：新句柄，旧句柄，新图裁剪点XY,旧图裁剪点XY,新图（拿出来的图）大小，旧图大小
+        imagecopyresampled($this->new,$this->img,0,0,$this->sx,$this->sy,
+                            $this->dw,$this->dh,$this->sw,$this->sh);
+    }
+	
+//-------------------------------------------
+
+
+ 
+  
+    public function close(){
+        imagedestroy($this->img);//消毁旧图资源句柄
+    }
+    public function delete(){
+        unlink($this->source);   
+    }
+}
+  
+?>
---
+++ Upload.php
@@ -0,0 +1,253 @@
+<?php
+namespace yxdj\filesystem;
+
+
+//上传文件类
+
+/*
+这里仅处理对单个文件值的上传处理，
+多个文件上传，对值的确定验证和格式化，在外部处理好之后再调用
+因为这些是不确定的，把它放在接口处更加灵活
+
+$file=new FlyUpload();
+$file->upload($file,$root,$cut,$rname,$rsize,$rtypes);
+if($file->status){
+	$result['status']='ok';
+	$result['url']=$cutpre.$file->main;
+	$result['infox']=$file->disk;
+}else{
+	$result['status']='ng';
+	$result['info']=$file->error;
+}
+$out=json_encode($result);
+echo $out;
+
+
+
+
+
+//如果是多文件上传，指单请求多文件
+/*
+经表单直接提交测试，其选多文件时即单请求多文件
+命名为file,后面的覆盖前面的，
+命名为file[],看到的现象是php并不会将单个文件的数据存放一块，而是文件名全在一块，类型全在一块，，
+可能的接收方式：file['filename'][],file['size'][],或许这和多选框的上传是一致的
+不是想象中的:file['name']['ffilename',...]
+
+所以需要自已在这格式化成相应的文件数据再上传，
+不断是哪种格式，如果多文件，但我只想要一个呢，如果将由类文件处理，很难满足需求变更，参数更复杂
+在接口处处理，更灵活，上传类也更清淅
+*/
+
+
+
+//如果是大文件分段上传，指单文件多请求
+/*
+需要客端一个标识，可以是服务端上传的文件名及路径，可加密处理
+这个标识仅靠sessionID,虽然可在session里存放标识，
+但客户端每次的请求都是独立的，它要断开上传，重新上传，，这个在session里没法响应
+可以在客户端设置一个cookie，标识一个特定的文件要上传，这样服务端能根据cookie做出判断
+
+在这个过程中客户端是主动的且是多状态的，服务端是被动的。所以采用上述方式较好。
+*/
+
+
+
+/*
+$file=new FlyUpload();
+$filedata=$_FILES['file'];
+//root,cut可以自行组织，root是参考路径不会动到，cut会截断处理
+//返回的后台总是正常的，cut不是从根目录开始的则要自行剪接
+//前台的路径用于在前台显示
+//后台的路径，可能也需传向前台，之后回传后台接着处理，如点位处理
+//把后台全传过去可能会公开结构了，可以只传后一段，前面的到后台再接上，
+//这样只传完整的前台路径即可，前台跨域的话，要加上域名,
+//这里的路径只要保证每一段被/或\分隔就行，内部会自行处理
+//用SERVER变量可以自动侦测路径，写死也是可以的,但移动目录就会出错
+//标准写法
+//$root=$_SERVER['DOCUMENT_ROOT'];
+//$cut=dirname($_SERVER['SCRIPT_NAME']).'/upload/'.date('Ymd').'/';
+
+//安全写法,让更少的路径在cut中，这是要处理的部分;
+//cutpre是省略的部分，后续要加在返回的前台路径上
+$root=$_SERVER['DOCUMENT_ROOT'].'/'.dirname($_SERVER['SCRIPT_NAME']).'/upload/';
+$cut=date('Ymd').'/';
+$cutpre=preg_replace('#[\\\/]+#','/','/'.dirname($_SERVER['SCRIPT_NAME']).'/upload');
+
+//文件名，提供后缀则以此后缀为文件名，否则就用上传文件的后缀（一般也就是这样），
+//真实的类弄不读取文件内容是没办法知道的，浏览器也仅仅根据后缀判断类型而已，
+$filename=date('YmdHis').mt_rand(1,999999);
+
+//服务端能接收的文件最大byte限制,如果php配置层有更严格的限制，这个值没用
+//客服务端的大小限制没用，需要的话仅在前端做更严格的限制即可，
+//PHP可能会接收客服端的大小做判断，给出的结果处理过程也接收，但完全没必要在PHP代码里做处理
+$size_s=3400000;
+
+//可接收的上传文件类开型，不要点，仅根据后缀判断而已
+$type=array('jpg','gif','png','zip','rar','doc','xls','ppt');
+
+//(文件值,根路径,主目录,文件名,,最大值,允许当传的文件类型)
+$file->upload($filedata,$root,$cut,$filename,$size_s,$type);
+
+//根据上传结束后生成的状态生成返回结果
+if($file->status){
+	$result['status']='ok';
+	$result['url']=$cutpre.$file->main;
+	$result['infox']=$file->disk;
+}else{
+	$result['status']='ng';
+	$result['info']=$file->error;
+}
+
+//将结果JSON化并输出
+$out=json_encode($result);
+echo $out;
+*/
+
+class Upload {
+	
+	private $fsize;				//上传文件实际大小	
+	private $ferror;			//错误代码	
+	private $ftype;				//上传文件类型
+	private $fname;				//上传文件名
+	private $ftmp;				//上传临时文件名
+	
+	private $rsize;    	 		//服务器程序最大值server(有时服务器配置也无权修改)		
+	private $rname;				//文件名
+	private $rtypes;			//类型合集	
+	private $rootpath;			//根目录
+	private $cutpath;			//中间目录	
+
+	
+	public  $main;			//含文件名，返回前台./filename
+	public  $disk;			//含文件名，后台G:/filename	
+	public 	$status;			//处理状态
+	public 	$error;				//错误信息
+
+	
+	//构造方法，初始化
+	public function upload($file,$root,$cut,$rname,$rsize,$rtypes){
+		//初始化状态
+		$this->init();
+		
+		//来源参数
+		$this->fsize=$file['size'];
+		$this->ferror = $file['error'];	
+		$this->fname = $file['name'];
+		$this->ftmp = $file['tmp_name'];
+		$this->ftype = trim(strrchr($this->fname,'.'),'.');
+		
+		//可接收的参数
+		$this->rsize = $rsize;
+		$this->rtypes=$rtypes;		
+		$this->rname=strpos($rname,'.')?$rname:$rname.'.'.$this->ftype;	
+			
+		//传进来的路径保证每一段至少有一了/或\就行
+		//这里对root,cut做严格的处理，对传进来的参数过滤，方便文件生成，及对外输出
+		$this->rootpath=preg_replace('#[\\\/]+#','/',$root.'/');		
+		$this->cutpath=ltrim(preg_replace('#[\\\/]+#','/',$cut.'/'),'/');	
+				
+		//返回给前后台用的路径，如果main不是script_name,那返回后，前台自行剪接
+		$this->main='/'.$this->cutpath.$this->rname;
+		$this->disk=$this->rootpath.$this->cutpath.$this->rname;
+		
+		
+		if(!$this->checkError()){return false;}
+		if(!$this->checkType()){return false;}
+		if(!$this->createUploadPath()){return false;}
+		if(!$this->moveUpload()){return false;}
+		$this->status=true;
+		$this->error='upload success!';
+		return true;
+	}
+
+
+	
+	//初始化状态，去除上次残留消息
+	private function init(){
+		$this->status=false;
+		$this->error='init: upload fail!';
+	}
+
+
+	
+	//验证错误
+	private function checkError(){
+		$error=$this->ferror;
+		if($this->fsize>$this->rsize ){$error=5;}
+		if (!empty($error)){
+			switch ($error){
+				case 1 :$this->error='上传值超过了约定最大值！';break;
+				case 2 :$this->error='文件过大-b！';break;
+				case 3 :$this->error='只有部分文件被上传！';break;
+				case 4 :$this->error='没有任何文件被上传！';break;
+				case 5 :$this->error='文件过大-s！';break;
+				default:$this->error='未知错误！';					
+			}
+			return false;
+		}
+		return true;
+	}
+
+	//验证类型
+	private function checkType(){
+		if (!in_array($this->ftype,$this->rtypes)) {
+			$this->error='不合法的上传类型！'.$this->ftype;
+			return false;			
+		}
+		return true;
+	}	
+	
+	//创建上传文件目录
+	private function createUploadPath(){
+		if(!$this->createDir($this->rootpath,$this->cutpath)){
+			$this->error='目录创建失败！';
+			return false;			
+		}
+		return true;
+	}
+
+
+	//目录创建方法
+	//只传一个参数时即为cut,root为空
+	private function createDir($root,$cut=''){
+			if($cut){
+				$cut=trim($cut,'/').'/';
+				$root=rtrim($root,'/').'/';
+			}else{
+				$cut=rtrim($root,'/').'/';	
+				$root='';
+			}
+
+			
+			//提取路径节点
+			for($i=0;$i=strpos($cut,'/',++$i);){$dirs[]=substr($cut,0,$i);}
+			//创建目录
+			for($i=0;$i<count($dirs);$i++){
+				if (!is_dir($root.$dirs[$i]) || !is_writable($root.$dirs[$i])) {
+					if (!mkdir($root.$dirs[$i])) {//当目录不存在，或其不可写时创建一个
+						return false;
+					}
+				}
+			}
+			return true;
+	}			
+		
+
+	
+	//移动文件
+	private function moveUpload(){
+		if (is_uploaded_file($this->ftmp)) {
+			if (!move_uploaded_file($this->ftmp,$this->disk)){
+				$this->error='上传失败！';
+				return false;	
+			}else{
+				return true;
+			}
+		} else {
+			$this->error='临时文件不存在！';
+			return false;			
+		}
+	}
+
+}
